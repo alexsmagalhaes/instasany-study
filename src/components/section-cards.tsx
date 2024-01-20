26f2a0de185be917a5c1a2e3726f9b716486e213
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-01-20 15:10:51 -0300

    Gsap console error resolve

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -17,8 +17,6 @@ import Card06 from '/public/image-06.png'
 
 function SectionCards(): ReactNode {
 
-   gsap.registerPlugin(ScrollTrigger);
-
    const mockupPhoneRef = useRef(null);
    const titleRef = useRef(null);
    const sectionTriggerRef = useRef(null)
@@ -34,6 +32,8 @@ function SectionCards(): ReactNode {
    }
 
    useEffect(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
       gsap.fromTo(mockupPhoneRef.current,
          { opacity: 0, scale: 0.5, },
          { opacity: 1, scale: 1, duration: 1, ease: 'power4.out' }
@@ -66,12 +66,12 @@ function SectionCards(): ReactNode {
                Faça <span className="text-green-highlight">você</span> mesmo de casa
             </h2>
             <div className="relative w-full max-w-home-cards-area mx-auto h-home-cards-lenght">
-               <Image ref={cardRef[0]} className="absolute opacity-0 top-8 left-44" src={Card01} alt="Card01" />
-               <Image ref={cardRef[1]} className="absolute opacity-0 left-0 bottom-32" src={Card02} alt="Card02" />
-               <Image ref={cardRef[2]} className="absolute opacity-0 bottom-0 left-80" src={Card03} alt="Card03" />
-               <Image ref={cardRef[3]} className="absolute opacity-0 top-0 right-32" src={Card04} alt="Card04" />
-               <Image ref={cardRef[4]} className="absolute opacity-0 right-0 bottom-28" src={Card05} alt="Card05" />
-               <Image ref={cardRef[5]} className="absolute opacity-0 bottom-0 right-80" src={Card06} alt="Card06" />
+               <div ref={cardRef[0]} className="absolute opacity-0 top-8 left-44"><Image src={Card01} alt="Card01" /></div>
+               <div ref={cardRef[1]} className="absolute opacity-0 left-0 bottom-32"><Image src={Card02} alt="Card02" /></div>
+               <div ref={cardRef[2]} className="absolute opacity-0 bottom-0 left-80"><Image src={Card03} alt="Card03" /></div>
+               <div ref={cardRef[3]} className="absolute opacity-0 top-0 right-32"><Image src={Card04} alt="Card04" /></div>
+               <div ref={cardRef[4]} className="absolute opacity-0 right-0 bottom-28" ><Image src={Card05} alt="Card05" /></div>
+               <div ref={cardRef[5]} className="absolute opacity-0 bottom-0 right-80"><Image src={Card06} alt="Card06" /></div>
             </div>
          </GridContainer>
       </section>
